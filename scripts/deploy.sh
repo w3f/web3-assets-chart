e27b57665aeff305574f732e1f67cdcb03252ba2
pantelis <pampatzoglou@gmail.com>
2020-05-19 12:08:01 +0300

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -ex
+ENCODED_TOKEN=$(echo -n "$TOKEN" | base64 -w0 );
 
 /scripts/deploy.sh -t helm -c engineering -a "\
  --set rclone.config.driveName=$DRIVE_NAME\
  --set rclone.config.scope=$DRIVE_SCOPE\
  --set rclone.config.rootFolderID=$ROOT_FOLDER_ID\
- --set rclone.config.token=$TOKEN\
+ --set rclone.config.token=$ENCODED_TOKEN\
  --set rclone.config.github=$GITHUB_BOT_TOKEN\
  assets --namespace=assets w3f/assets"
